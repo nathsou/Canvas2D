96019ac6268152d731b9419b4387357ae654a508
nathsou <Workisdaft3)>
2015-06-23 16:11:19 +0200

    Minor imporvements

--- src/fr/nathsou/Canvas2D/PixelManipulation/CanvasFilters.java
+++ src/fr/nathsou/Canvas2D/PixelManipulation/CanvasFilters.java
@@ -14,50 +14,53 @@ import java.io.IOException;
 public abstract class CanvasFilters {
 
     public static Canvas2D negative(Canvas2D cnv) {
+        Canvas2D copy = cnv;
 
         int brightness;
-        for (int x = 0; x < cnv.getWidth(); x++) {
-            for (int y = 0; y < cnv.getHeight(); y++) {
-                Color c = cnv.getRGB(x, y);
-                cnv.setRGB(x, y, new Color(255 - c.getRed(), 255 - c.getGreen(), 255 - c.getBlue()));
+        for (int x = 0; x < copy.getWidth(); x++) {
+            for (int y = 0; y < copy.getHeight(); y++) {
+                Color c = copy.getRGB(x, y);
+                copy.setRGB(x, y, new Color(255 - c.getRed(), 255 - c.getGreen(), 255 - c.getBlue()));
             }
         }
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D grayScale(Canvas2D cnv) {
 
+        Canvas2D copy = cnv;
         int brightness;
-        for (int x = 0; x < cnv.getWidth(); x++) {
-            for (int y = 0; y < cnv.getHeight(); y++) {
-                Color c = cnv.getRGB(x, y);
+        for (int x = 0; x < copy.getWidth(); x++) {
+            for (int y = 0; y < copy.getHeight(); y++) {
+                Color c = copy.getRGB(x, y);
                 brightness = (int) (0.21f * c.getRed() + 0.72f * c.getGreen() + 0.07f * c.getBlue()); //Luminosity scaling
-                cnv.setRGB(x, y, new Color(brightness, brightness, brightness));
+                copy.setRGB(x, y, new Color(brightness, brightness, brightness));
             }
         }
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D naiveTreshold(Canvas2D cnv, int fluct) {
+        Canvas2D copy = cnv;
         int s = 0;
-        cnv = grayScale(cnv);
-        for (Color c : grayScale(cnv).getPixels()) {
+        copy = grayScale(copy);
+        for (Color c : grayScale(copy).getPixels()) {
             s += c.getRed();
         }
-        float avgColor = s / cnv.getPixels().size();
+        float avgColor = s / copy.getPixels().size();
 
 
-        for (int x = 0; x < cnv.getWidth(); x++) {
-            for (int y = 0; y < cnv.getHeight(); y++) {
-                Color c = cnv.getRGB(x, y);
+        for (int x = 0; x < copy.getWidth(); x++) {
+            for (int y = 0; y < copy.getHeight(); y++) {
+                Color c = copy.getRGB(x, y);
                 int b = c.getRed();
-                cnv.setRGB(x, y, (b > avgColor - fluct) ? Color.white : Color.black);
+                copy.setRGB(x, y, (b > avgColor - fluct) ? Color.white : Color.black);
             }
         }
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D cannyEdge(Canvas2D cnv) throws IOException {
@@ -70,18 +73,19 @@ public abstract class CanvasFilters {
     }
 
     public static Canvas2D treshold(Canvas2D cnv, int sq, float fluct) {
-        cnv = grayScale(cnv);
+        Canvas2D copy = cnv;
+        copy = grayScale(copy);
         Point p1, p2;
-        for (int y = sq; y <= cnv.getHeight() + sq; y += sq) {
-            for (int x = 0; x <= cnv.getWidth(); x += sq) {
+        for (int y = sq; y <= copy.getHeight() + sq; y += sq) {
+            for (int x = 0; x <= copy.getWidth(); x += sq) {
                 p1 = new Point(x, y - sq);
                 p2 = new Point(x + sq, y);
 
-                Color avg = PixelTools.averageColor(p1, p2, cnv);
+                Color avg = PixelTools.averageColor(p1, p2, copy);
                 //cnv.fillRegion(p1, p2, avg);
                 for (int y2 = p1.y; y2 < p2.y; y2++) {
                     for (int x2 = p1.x; x2 < p2.x; x2++) {
-                        cnv.setRGB(x2, y2, (PixelTools.colorDistance(cnv.getRGB(x2, y2), avg) < fluct)?Color.white:Color.black);
+                        copy.setRGB(x2, y2, (PixelTools.colorDistance(copy.getRGB(x2, y2), avg) < fluct)?Color.white:Color.black);
                     }
                 }
 
@@ -89,28 +93,29 @@ public abstract class CanvasFilters {
         }
 
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D pixelate(Canvas2D cnv, int sq, boolean grid) {
 
+        Canvas2D copy = cnv;
         //int sq = (int) Math.sqrt(cnv.getPixels().size() / nbPixel);
         Point p1, p2;
-        for (int y = sq; y <= cnv.getHeight() + sq; y += sq) {
-            for (int x = 0; x <= cnv.getWidth(); x += sq) {
+        for (int y = sq; y <= copy.getHeight() + sq; y += sq) {
+            for (int x = 0; x <= copy.getWidth(); x += sq) {
                 p1 = new Point(x, y - sq);
                 p2 = new Point(x + sq, y);
 
-                Color avg = PixelTools.averageColor(p1, p2, cnv);
-                cnv.fillRegion(p1, p2, avg);
+                Color avg = PixelTools.averageColor(p1, p2, copy);
+                copy.fillRegion(p1, p2, avg);
 
                 if (grid) {
                     for (int y2 = p1.y; y2 < p2.y; y2++) {
                         for (int x2 = p1.x; x2 < p2.x; x2++) {
                             //cnv.setRGB(x2, y2, avg);
-                            cnv.drawLine(p1, new Point(p2.x, p1.y));
-                            cnv.drawLine(p1, new Point(p1.x, p2.y));
-                            cnv.drawLine(new Point(p1.x, p2.y), p2);
+                            copy.drawLine(p1, new Point(p2.x, p1.y));
+                            copy.drawLine(p1, new Point(p1.x, p2.y));
+                            copy.drawLine(new Point(p1.x, p2.y), p2);
                         }
                     }
                 }
@@ -119,7 +124,7 @@ public abstract class CanvasFilters {
         }
 
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D pixelate(Canvas2D cnv, int sq) {
@@ -129,26 +134,32 @@ public abstract class CanvasFilters {
 
     public static Canvas2D filterColor(Canvas2D cnv, Color c, float sigma){
 
-        for (int i = 0; i < cnv.getPixels().size(); i++) {
-            cnv.setNthPixel(i, (PixelTools.colorDistance(c, cnv.getPixels().get(i)) < sigma)?cnv.getPixels().get(i):Color.white);
+        Canvas2D copy = cnv;
+
+        for (int i = 0; i < copy.getPixels().size(); i++) {
+            copy.setNthPixel(i, (PixelTools.colorDistance(c, copy.getPixels().get(i)) < sigma)?copy.getPixels().get(i):Color.white);
         }
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D filterColor(Canvas2D cnv, Color c, Color replacementColor, float sigma) {
 
-        for (int i = 0; i < cnv.getPixels().size(); i++) {
-            cnv.setNthPixel(i, (PixelTools.colorDistance(c, cnv.getPixels().get(i)) < sigma)?cnv.getPixels().get(i):replacementColor);
+        Canvas2D copy = cnv;
+
+        for (int i = 0; i < copy.getPixels().size(); i++) {
+            copy.setNthPixel(i, (PixelTools.colorDistance(c, copy.getPixels().get(i)) < sigma)?copy.getPixels().get(i):replacementColor);
         }
 
-        return cnv;
+        return copy;
     }
 
     public static Canvas2D replaceColor(Canvas2D cnv, Color toReplace, Color replacement, int sigma){
 
-        for (int i = 0; i < cnv.getPixels().size(); i++) {
-            cnv.setNthPixel(i, (PixelTools.colorDistance(toReplace, cnv.getPixels().get(i)) < sigma)?replacement:cnv.getPixels().get(i));
+        Canvas2D copy = cnv;
+
+        for (int i = 0; i < copy.getPixels().size(); i++) {
+            copy.setNthPixel(i, (PixelTools.colorDistance(toReplace, cnv.getPixels().get(i)) < sigma)?replacement:cnv.getPixels().get(i));
         }
 
         return cnv;
--- src/fr/nathsou/Main.java
+++ src/fr/nathsou/Main.java
@@ -12,37 +12,13 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static int r = 0;
-    public static ArrayList<Color> dp;
 
     public static void main(String[] args) throws IOException {
 
         Canvas2D canvas = new Canvas2D(new Pixmap(new File("/Users/nathansoufflet/Pictures/laloue.jpeg")));
+
         CanvasFrame frame = new CanvasFrame(canvas);
         frame.setVisible(true);
-
-        dp = canvas.getPixels();
-
-
-        go(frame);
     }
 
-    public static void go(CanvasFrame cf){
-
-        r++;
-
-        System.out.println(r);
-        CanvasFilters.pixelate(cf.getCanvas(), r);
-        CanvasFilters.naiveTreshold(cf.getCanvas(), 10);
-        CanvasFilters.replaceColor(cf.getCanvas(), Color.white, Color.orange, 1);
-        cf.repaint();
-
-        try{
-            Thread.sleep(100);
-        }catch (InterruptedException ex){
-            ex.printStackTrace();
-        }
-        cf.getCanvas().setPixels(dp, cf.getCanvas().getWidth(), cf.getCanvas().getHeight());
-        go(cf);
-    }
 }
